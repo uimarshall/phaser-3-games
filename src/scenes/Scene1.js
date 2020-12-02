dcfc185fee38ab60f841338bd9573134ffa6c08a
uimarshall <uimarshall@gmail.com>
2020-12-02 15:33:08 +0100

    Add spitesheets and animations

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -13,11 +13,39 @@ class Scene1 extends Phaser.Scene {
 
   preload() {
     this.load.image('background', 'src/assets/images/background.png');
-    this.load.image('ship', 'src/assets/images/ship.png');
-    this.load.image('ship2', 'src/assets/images/ship2.png');
-    this.load.image('ship3', 'src/assets/images/ship3.png');
+    // Using image instead of spritesheet
+    // this.load.image('ship', 'src/assets/images/ship.png');
+    // this.load.image('ship2', 'src/assets/images/ship2.png');
+    // this.load.image('ship3', 'src/assets/images/ship3.png');
+
+    // ========================================================================
+  /**
+   * SPRITESHEET IS USED FOR ANIMATIONS IN PHASER
+   * A "SpriteSheet" is a coll of images in a single file separated by frames.
+   * Using Spritesheets is more expensive for the processor, hence use of images
+  */
+   this.load.spritesheet('ship', 'src/assets/spritesheets/ship.png',{
+       frameWidth: 16,//frame size
+       frameHeight:16
+     });
+   this.load.spritesheet('ship2', 'src/assets/spritesheets/ship2.png',{
+       frameWidth: 32,
+       frameHeight:16
+     });
+   this.load.spritesheet('ship3', 'src/assets/spritesheets/ship3.png',{
+       frameWidth: 32,
+       frameHeight:32
+     });
+   this.load.spritesheet('explosion', 'src/assets/spritesheets/explosion.png',{
+       frameWidth: 16,
+       frameHeight:16
+     });
+// ==========================================================================
+
+     
   }
 
+  
   // this.add.text - creates a text inside scene1
   // 20,20 reps the X&Y axis, then the text to display
   create() {
--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -13,20 +13,79 @@ class Scene2 extends Phaser.Scene {
     this.background.setOrigin(0, 0);
     this.add.text(20, 20, 'Playing game...', { font: '25px Arial', fill: 'yellow' });
 
-    this.ship1 = this.add.image(config.width / 2 - 50, config.height / 2, 'ship');
-    this.ship2 = this.add.image(config.width / 2, config.height / 2, 'ship2');
-    this.ship3 = this.add.image(config.width / 2 + 50, config.height / 2, 'ship3');
-    //  this.ship1.setScale(2)
+    // Declaration using image
+    // this.ship1 = this.add.image(config.width / 2 - 50, config.height / 2, 'ship');
+    // this.ship2 = this.add.image(config.width / 2, config.height / 2, 'ship2');
+    // this.ship3 = this.add.image(config.width / 2 + 50, config.height / 2, 'ship3');
+    
+    // Declaration using sprite
+    this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, 'ship');
+    this.ship2 = this.add.sprite(config.width / 2, config.height / 2, 'ship2');
+    this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, 'ship3');
+     this.ship1.setScale(2)
     //  this.ship1.flipY = true
     //  this.ship1.angle += 3//rotate continously
+// ===========================================================================
+    /** CREATING ANIMATIONS 
+     *  this.anims.create({key, frames, frameRate, repeat})
+     * key - Is the id for the Animation
+     * frames - Is an array of frames
+     * frameRate - Is the speed of the Animation
+     * repeat - will it loop?
+    */
+   this.anims.create({
+     key:"ship1_anim",//create animation named ship1_anim
+     frames: this.anims.generateFrameNumbers("ship"),//using the spritesheets frame
+     frameRate:20,
+     repeat:-1// -1 is for infinite loop
+ 
+   })
+   this.anims.create({
+     key:"ship2_anim",//create animation named ship1_anim
+     frames: this.anims.generateFrameNumbers("ship2"),//using the ship spritesheets frame
+     frameRate:20,
+     repeat:-1// -1 is for infinite loop
+ 
+   })
+   this.anims.create({
+     key:"ship3_anim",//create animation named ship1_anim
+     frames: this.anims.generateFrameNumbers("ship3"),//using the spritesheets frame
+     frameRate:20,
+     repeat:-1// -1 is for infinite loop
+ 
+   })
+   this.anims.create({
+     key:"explode",//create animation named ship1_anim
+     frames: this.anims.generateFrameNumbers("explosion"),//using the ship spritesheets frame
+     frameRate:20,
+     repeat:0,// 0 is for it to occur once.
+     hideOnComplete:true//for the explosion animation to disappear once completed.
+     
+ 
+   })
+   this.ship1.play("ship1_anim")
+   this.ship2.play("ship2_anim")
+   this.ship3.play("ship3_anim")
 
+  //  On clicking the ship, it should explode
+  this.ship1.setInteractive()
+  this.ship2.setInteractive()
+  this.ship3.setInteractive()
+
+  // Add EventListener
+  this.input.on("gameobjectdown", this.destroyShip, this)
+ // =========================================================================
+   
+// ========================================================================
   /** 
    * USE tileSprite instead og image
    *  A TileSprite is a Sprite that has a repeating texture,
    * It used when you want to move the texture rather than the image.
   */
   // this.background = this.add.tileSprite(0, 0,config.width, config.height, 'background');
-  }
+  
+  
+}
 
   /** 2 params,the 'ship' obj to be move and the Y-velocity of the ship */
   moveShip(ship,speed){
@@ -58,6 +117,12 @@ class Scene2 extends Phaser.Scene {
     let randomX = Phaser.Math.Between(0, config.width)
     ship.x = randomX
 
+  }
+
+  // callback func to destroy ship
+  destroyShip(pointer, gameObject){
+    gameObject.setTexture("explosion")
+    gameObject.play("explode")
   }
 
  
