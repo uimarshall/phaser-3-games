b07f41a6f354ea1ac8ba96a0cb81e5066a743de8
uimarshall <uimarshall@gmail.com>
2020-12-02 12:50:59 +0100

    Add motions to images and tileSprite.
    The tileSprite is moving the texture, the background.

--- src/scenes/Scene2.js
+++ src/scenes/Scene2.js
@@ -8,7 +8,8 @@ class Scene2 extends Phaser.Scene {
 
   create() {
     // set a variable called 'this.background'
-    this.background = this.add.image(0, 0, 'background');
+    // this.background = this.add.image(0, 0, 'background');
+    this.background = this.add.tileSprite(0, 0,config.width, config.height, 'background');
     this.background.setOrigin(0, 0);
     this.add.text(20, 20, 'Playing game...', { font: '25px Arial', fill: 'yellow' });
 
@@ -18,6 +19,49 @@ class Scene2 extends Phaser.Scene {
     //  this.ship1.setScale(2)
     //  this.ship1.flipY = true
     //  this.ship1.angle += 3//rotate continously
+
+  /** 
+   * USE tileSprite instead og image
+   *  A TileSprite is a Sprite that has a repeating texture,
+   * It used when you want to move the texture rather than the image.
+  */
+  // this.background = this.add.tileSprite(0, 0,config.width, config.height, 'background');
   }
+
+  /** 2 params,the 'ship' obj to be move and the Y-velocity of the ship */
+  moveShip(ship,speed){
+    ship.y += speed
+    if (ship.y > config.height) {
+      this.resetShipPosition(ship)
+      
+    }
+
+  }
+
+  update(){
+    this.moveShip(this.ship1, 1)
+    this.moveShip(this.ship2, 2)
+    this.moveShip(this.ship3, 3)
+    // decrease the position of the texture of the bg
+    this.background.tilePositionY -= 0.5
+
+  }
+
+  /**
+   * Create a func that resets the positions of the ship to the top of the screen
+   * after falling off due to the updated speed in update()
+   * Y-axis = 0; X-axis = random position.
+   */
+
+   resetShipPosition(ship){
+    ship.y = 0
+    let randomX = Phaser.Math.Between(0, config.width)
+    ship.x = randomX
+
+  }
+
+ 
+
+ 
 }
 export default Scene2;
